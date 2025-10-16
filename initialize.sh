bde2622f16d8f076eb790f9c57ad440baceb21de
Luca Domeneghetti <lucadomeneghetti2002@gmail.com>
2025-10-16 11:00:01 +0200

    updated initialization script

--- initialize.sh
+++ initialize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/lucadomene/DFB_Lab01_DataCarving /opt/datacarving-lab
+git clone -b students --single-branch https://github.com/lucadomene/DFB_Lab01_DataCarving /opt/datacarving-lab
 losetup -fP /opt/datacarving-lab/ext4.dd
 rm -rf /content/sample_data
 cp -r /opt/datacarving-lab/disk_images /content
