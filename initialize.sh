b77a6e706ea76a1581a1771e2c785e4ad63b49f2
Luca Domeneghetti <lucadomeneghetti2002@gmail.com>
2025-10-16 11:19:38 +0200

    corrected initialization script

--- initialize.sh
+++ initialize.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 git clone -b students --single-branch https://github.com/lucadomene/DFB_Lab01_DataCarving /opt/datacarving-lab
-losetup -fP /opt/datacarving-lab/ext4.dd
+ln -s /dev/loop0 /opt/datacarving-lab/ext4.dd
 rm -rf /content/sample_data
 cp -r /opt/datacarving-lab/disk_images /content
 mkdir -p /content/retrieved_data
+
+apt install dcfldd sleuthkit xxd binwalk scalpel 
