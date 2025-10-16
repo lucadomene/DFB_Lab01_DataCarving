7428246c8ffc79b3c19a8c9f821cb4e69a4c2f36
Luca Domeneghetti <lucadomeneghetti2002@gmail.com>
2025-10-16 10:51:00 +0200

    Revert "added initialization script"
    This reverts commit df74d06142bd48cc4fce91e33dcad72cb3c0333c.

--- initialize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/lucadomene/DFB_Lab01_DataCarving /opt/datacarving-lab
-losetup -fP /opt/datacarving-lab/ext4.dd
-rm -rf /content/sample_data
-cp -r /opt/datacarving-lab/disk_images /content
-mkdir -p /content/retrieved_data
