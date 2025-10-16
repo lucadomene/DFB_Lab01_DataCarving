1d7b8b88a0b489d1dfcceaeb0ea79da9636c3ff8
Luca Domeneghetti <lucadomeneghetti2002@gmail.com>
2025-10-16 18:39:36 +0200

    updated script

--- initialize.sh
+++ initialize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git clone -b students --single-branch https://github.com/lucadomene/DFB_Lab01_DataCarving /opt/datacarving-lab
-ln -s /dev/loop0 /opt/datacarving-lab/ext4.dd
+ln -s /opt/datacarving-lab/ext4.dd /dev/loop0 
 rm -rf /content/sample_data
-cp -r /opt/datacarving-lab/disk_images /content
+cp -r /opt/datacarving-lab/challenges /content
 mkdir -p /content/retrieved_data
 
 apt install dcfldd sleuthkit xxd binwalk scalpel 
