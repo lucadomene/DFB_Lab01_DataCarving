3d811d06752e8d85a6eb3adbf52f0e8ced5c681e
Luca Domeneghetti <lucadomeneghetti2002@gmail.com>
2025-10-17 15:06:53 +0200

    updated script

--- initialize.sh
+++ initialize.sh
@@ -8,5 +8,6 @@ mkdir -p /content/retrieved_data
 
 apt install dcfldd sleuthkit xxd scalpel
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-PATH=/root/.cargo/bin:$PATH
 cargo install binwalk
+PATH=/root/.cargo/bin:$PATH
+source "root/.cargo/env"
